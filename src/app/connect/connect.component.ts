e33149bda58d866ba5d99e3e6eeb05e93a4b0b14
mkntr <nair.mohanakrishnantr@gmail.com>
2023-03-30 10:30:50 +0530

    loading new api key

--- src/app/connect/connect.component.ts
+++ src/app/connect/connect.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
   styleUrls: ['./connect.component.css']
 })
 export class ConnectComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
